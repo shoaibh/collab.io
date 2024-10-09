7fda2a6ef2f03d419c9fe29277a0451c564d2684
Apple <apple@cap-rx.com>
2024-10-10 01:40:35 +0530

    update loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { UserButton } from "@/features/auth/components/user-button";
-import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
-import { useEffect, useMemo } from "react";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
   const router = useRouter();
@@ -24,8 +24,8 @@ export default function Home() {
     }
   }, [workspaceId, isLoading, createWorkspaceModal, router, setCreateWorkspaceModal]);
   return (
-    <div>
-      <UserButton />
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
 }
